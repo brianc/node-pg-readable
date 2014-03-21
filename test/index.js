6b62a3dd9ce5339ede808c89a7d25f0e6afd326c
Brian M. Carlson <brian.m.carlson@gmail.com>
2014-03-21 11:52:05 -0500

    Bump pg-query-stream version
    Also add test for slow consumer

--- test/index.js
+++ test/index.js
@@ -10,11 +10,25 @@ it('works', function(done) {
   })
 })
 
-it('works with passed in constructor', function() {
+it('works with passed in constructor', function(done) {
   var stream = pgreader(require('pg.js').Client, 'SELECT 1::int as num')
   stream.once('readable', function() {
     var row = stream.read()
     assert.equal(row.num, 1)
     done()
+  })
+})
+
+var reader = require('stream-reader')
+var batch = new (require('batch-stream'))({size: 37})
+it('works with slow consumer', function(done) {
+  var stream = pgreader('SELECT * FROM generate_series(0, 2000) num', [])
+  var count = 0
+  reader(stream.pipe(batch), function(rows, cb) {
+    count += rows.length
+    if(count == 2001) {
+      done()
+    }
+    setTimeout(cb, 10)
   })
 })
